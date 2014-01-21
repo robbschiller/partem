aa4690bab6286367fc3a55e5b8d90390968fb455
Robb Schiller <robb.schiller@gmail.com>
2014-01-21 12:37:10 -0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var gulp = require("gulp")
+var sass = require("gulp-sass")
+var jshint = require("gulp-jshint")
+var nodemon = require("gulp-nodemon")
+
+gulp.task('sass', function () {
+	gulp.src('./public/scss/main.scss')
+		.pipe(sass(includePath: './public/scss'))
+		.pipe(gulp.dest('./public/css')
+});
+
+gulp.task('lint', function () {
+	gulp.src('.public/js/**.js')
+		.pipe(jshint())
+});
+
+gulp.task('default', function () {
+	gulp.run('sass');
+
+	gulp.watch('./public/scss/**.scss', function () {
+		gulp.run('sass');
+	});
+});
+
+gulp.task('develop', function () {
+	nodemon({script: 'server.js'})
+		.on('restart', ['lint', 'sass'])
+});
