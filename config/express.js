2115832db542202bcaae3012799dbc489b61ff7d
Robb Schiller <robb.schiller@gmail.com>
2014-02-27 09:14:21 -0500

    updates

--- config/express.js
+++ config/express.js
@@ -6,8 +6,18 @@ var express = require('express'),
 
 module.exports = function(app) {
 
+	// Express Compress
+	app.use(express.compress({
+		filter: function(req, res) {
+			return (/json|test|javascript|css/).test(res.getHeader('Content-Type'));
+		},
+		level: 9
+	}));
+
+	// Express Static Dir
 	app.use(express.static(config.root + '/public'));
 
+	// Set View path and Layout Engine
 	app.set('views', config.root + '/app/views');
 	app.set('view engine', 'jade');
 
